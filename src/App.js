a82356e2b64215c33a55dc32a46a54d0eab0bd94
Arafat Olayiwola <harof.dev@gmail.com>
2021-04-23 05:21:27 +0100

    First commit of this reactjs todolist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Hello from './Hello/Hello'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +17,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <Hello name="Arafat" />
+
       </header>
     </div>
   );
